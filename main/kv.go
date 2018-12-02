a13eabcd0a7ea036750038ad49f28b9b0047e39f
Kevin Cai <kevincai96@gmail.com>
2018-12-02 10:27:41 -0500

    Attempt DB recovery if corrupt

--- main/kv.go
+++ main/kv.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/errors"
 )
 
 func openDB() {
 	var err error
 	db, err = leveldb.OpenFile(baseDir, nil)
 	if err != nil {
-		panic(err)
+		if errors.IsCorrupted(err) {
+			db, err = leveldb.RecoverFile(baseDir, nil)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
 	}
 }
